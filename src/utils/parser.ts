0855ce30a2ed012b323907f9408017f0c6191477
Ishan Khanna <ishan.khanna@proptiger.com>
2022-01-05 18:07:35 +0530

    fixing parser

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,11 +1,10 @@
 export function parseFieldsFromObject({ object }) {
     const ans = [];
     Object.keys(object).forEach(key => {
+        ans.push(key);
         if (Object.keys(object[key]).length) {
             const recAns = parseFieldsFromObject({ object: object[key] });
             ans.push(...recAns);
-        } else {
-            ans.push(key);
         }
     });
     return [...new Set(ans)];
